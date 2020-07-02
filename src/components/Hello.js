60995c4e88ae1cd2278fa39312b56f306a1c8a77
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-07-02 18:15:50 +0530

    without jsx component and learned about React.create element and about it

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,11 +1,23 @@
 import React from 'react';
 
+// this is the jsx version of hello component
+// const Hello = () => {
+//     return (
+//         <div>
+//             <h1>Hello Nilank</h1>
+//         </div>
+//     )
+// }
+
+// Writing without jsx componet
 const Hello = () => {
-    return (
-        <div>
-            <h1>Hello Nilank</h1>
-        </div>
-    )
+    // return (
+    //     <div>
+    //         <h1>Hello Nilank</h1>
+    //     </div>
+    // )
+    return React.createElement('div', null, 'Hello Nilank')
 }
+
 
 export default Hello;
