89da323fccc10313b918f6f9507bf55bec198d24
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-07-02 23:10:43 +0530

    passed on more props

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ import Hello from './components/Hello';
 function App() {
   return (
     <div className="App">
-      <Greet name="Harshita"/>
-      <Greet name="annadanapu"/>
-      <Greet name="adarsh"/>
+      <Greet name="Bruce" heroname="Batman"/>
+      <Greet name="Clark" heroname="Superman"/>
+      <Greet name="Diana" heroname="Wonder Woman"/>
       {/* <Welcome /> */}
 
       {/* <Hello /> */}
--- src/components/Greet.js
+++ src/components/Greet.js
@@ -7,7 +7,7 @@ import React from 'react';
 // this props we can name it anything we want to, but conventional is to write props
 const Greet = (props) => {
     console.log("props",props);
-    return <h1>Hello {props.name}</h1>
+    return <h1>Hello {props.name} {props.heroname}</h1>
 }
 // this is termed as named export
 // export const Greet = () => <h1>Hello Nilank</h1>
