e8ee94cfaefb415a54a8946a79e5336c9513046b
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-07-03 16:19:50 +0530

    started learning about setstate

---
+++ src/components/Counter.js
@@ -0,0 +1,15 @@
+// since we are learning about state in class component this is going to be a class component
+
+import React, { Component } from 'react'
+
+class Counter extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Counter
