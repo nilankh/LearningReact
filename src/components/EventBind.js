39b6d1b770ebbdb93add6fb53b6aadcee7801ea5
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-07-03 20:23:55 +0530

    learning about binding event handler

--- src/components/EventBind.js
+++ src/components/EventBind.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react'
 
 class EventBind extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             message:"Hello"
+        }
+    }
+    clickHandler() {
+        this.setState({
+            message: 'Goodbye!'
+        })
+    }
     render() {
         return (
             <div>
-                
+                <div>{this.state.message}</div>
+                <button onClick={this.clickHandler} >Click</button>
             </div>
         )
     }
