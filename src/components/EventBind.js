ece53e30ae57dbd568230c2a70a5c29d2ee6cfdc
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-07-03 20:36:02 +0530

    learning about binding event handler way2

--- src/components/EventBind.js
+++ src/components/EventBind.js
@@ -16,13 +16,18 @@ class EventBind extends Component {
         // this keyword within our event handler is undefined and thaty event binding is necessary in react class components 
     }
     render() {
+        
         return (
             <div>
                 <div>{this.state.message}</div>
-                <button onClick={this.clickHandler.bind(this)} >Click</button>
+                 {/* approach1 in render method */}
+                {/* <button onClick={this.clickHandler.bind(this)} >Click</button> */}
+
+                {/* approach2 */}
+                <button onClick={()=> this.clickHandler()} >Click</button>
             </div>
         )
     }
 }
-
+// second method is to use arrow function in render method
 export default EventBind
