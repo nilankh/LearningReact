2381b93c210046028c66eb06a6edf71ed45edf4f
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-07-03 19:38:45 +0530

    learned about destructuing in class component

--- src/App.js
+++ src/App.js
@@ -16,18 +16,18 @@ function App() {
       {/* <Counter /> */}
 
       {/* <Message /> */}
-      <Greet name="Bruce" heroname="Batman"/>
-      {/* <Greet name="Bruce" heroname="Batman">
+      {/* <Greet name="Bruce" heroname="Batman"/>
+      <Greet name="Bruce" heroname="Batman">
         <p>This is children props</p>
       </Greet>
       <Greet name="Clark" heroname="Superman">
         <button>Action</button>
       </Greet>
       
-      <Greet name="Diana" heroname="Wonder Woman"/>
+      <Greet name="Diana" heroname="Wonder Woman"/> */}
       <Welcome name="Bruce" heroname="Batman" />
       <Welcome name="Clark" heroname="Superman" />
-      <Welcome name="Diana" heroname="Wonder Woman" /> */}
+      <Welcome name="Diana" heroname="Wonder Woman" />
 
       {/* <Hello /> */}
     </div>
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -2,14 +2,28 @@ import React, { Component } from 'react';
 // class component 
 
 
+// class Welcome extends Component {
+//     render() {
+//     return (
+//         <h1>
+//             Welcome {this.props.name} {this.props.heroname}
+//         </h1>
+//     )
+//     }
+// }
+
+
+// In class component we  generally tend to destruture props or state in render method
 class Welcome extends Component {
     render() {
-    return (
-        <h1>
-            Welcome {this.props.name} {this.props.heroname}
-        </h1>
-    )
+        const { name, heroname } = this.props
+        return (
+            <h1>
+                Welcome {name} {heroname}
+            </h1>
+        )
     }
 }
+
 
 export default Welcome;
