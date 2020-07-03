fd77ce33146d3a5383abceca8396045fdd9f2f87
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-07-03 15:22:11 +0530

    leanring about how state can be used in classed components created files Message.js

---
+++ src/components/Message.js
@@ -0,0 +1,17 @@
+// how state can be used in class components
+
+import React, { Component } from 'react';
+// class component 
+
+
+class Message extends Component {
+    render() {
+    return (
+        <h1>
+            Welcome Visitor
+        </h1>
+    )
+    }
+}
+
+export default Message;
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,7 +4,11 @@ import React, { Component } from 'react';
 
 class Welcome extends Component {
     render() {
-    return <h1>Welcome {this.props.name} {this.props.heroname}</h1>
+    return (
+        <h1>
+            Welcome {this.props.name} {this.props.heroname}
+        </h1>
+    )
     }
 }
 
