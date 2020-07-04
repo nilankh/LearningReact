60ecdce3a8116f8502bc31e1375bbd3885fbeecd
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-07-04 16:21:18 +0530

    started learning about conditional rendering

---
+++ src/components/UserGreeting.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class UserGreeting extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default UserGreeting
