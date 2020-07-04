fb6607ffcc0f2912fd529fa396990b1096e2f520
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-07-04 15:48:02 +0530

    started learning about method as props

--- src/components/ParentComponent.js
+++ src/components/ParentComponent.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react'
 
 class ParentComponent extends Component {
+
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             parentName: 'Parent'
+        }
+        this.greetParennt = this.greetParennt.bind(this)
+    }
+
+    greetParennt() {
+        alert(`Hello ${this.state.parentName}`)
+    }
+    
     render() {
         return (
             <div>
