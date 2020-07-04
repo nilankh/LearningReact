6ee8f78e1553bcf372cfecdf4a93cc0eb3996ea4
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-07-04 16:39:09 +0530

    Learning conditional rendering 3> ternary conditonal operator

--- src/components/UserGreeting.js
+++ src/components/UserGreeting.js
@@ -1,43 +1,46 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class UserGreeting extends Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
-        super(props)
-    
-        this.state = {
-             isLoggedIn: true
-        }
-    }
-    // I want to see the message If isLogged in is true then we have to display Welcome nilank otherwise welcome Guest
-    render() {
+    this.state = {
+      isLoggedIn: false,
+    };
+  }
+  // I want to see the message If isLogged in is true then we have to display Welcome nilank otherwise welcome Guest
+  render() {
+    // #rd approach Terniary Condtional Operator
+    return this.state.isLoggedIn ? (
+      <div>Welcome Nilank</div>
+    ) : (
+      <div>Welcome Guest</div>
+    );
 
+    // Second method Elements variables approach
+    // let message
 
-        // Second method Elements variables approach
-        let message 
+    // if(this.state.isLoggedIn){
+    //     message = <div>Welcome Nilank</div>
+    // }else{
+    //     message = <div>Welcome Guests</div>
+    // }
 
-        if(this.state.isLoggedIn){
-            message = <div>Welcome Nilank</div>
-        }else{
-            message = <div>Welcome Guests</div>
-        }
+    // return <div>{message}</div>
 
-        return <div>{message}</div>
-
-
-        // 1st method of condtional rendering
-        // if(this.state.isLoggedIn){
-        //     return (
-        //         <div>Welcome Nilank</div>
-        //     )
-        // }else {
-        //     return (
-        //         <div>
-        //             Welcome Guest
-        //         </div>
-        //     )
-        // }
-    }
+    // 1st method of condtional rendering
+    // if(this.state.isLoggedIn){
+    //     return (
+    //         <div>Welcome Nilank</div>
+    //     )
+    // }else {
+    //     return (
+    //         <div>
+    //             Welcome Guest
+    //         </div>
+    //     )
+    // }
+  }
 }
 
-export default UserGreeting
+export default UserGreeting;
