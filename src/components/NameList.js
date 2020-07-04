97b97ec97ac6e2b9d10f8d7ac30e139abdd57038
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-07-04 19:48:29 +0530

    learning list rendering by map method

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -4,10 +4,16 @@ function NamList() {
     const names = ['Brue', 'Clark', 'Diana']
     return (
         <div>
-           <h2>{names[0]}</h2> 
+            {/* 1st way to render  */}
+           {/* <h2>{names[0]}</h2> 
            <h2>{names[1]}</h2> 
-           <h2>{names[2]}</h2> 
+           <h2>{names[2]}</h2>  */}
            
+           {/* 2nd way to render via map method */}
+
+           {
+               names.map(name => <h2>{name}</h2>)
+           }
         </div>
     )
 }
