b950a93b5560a3cbf29937d9b420990519012e87
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-07-04 19:44:39 +0530

    learning about list rendering

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
 function NamList() {
+    const names = ['Brue', 'Clark', 'Diana']
     return (
         <div>
-            
+           <h2>{names[0]}</h2> 
+           <h2>{names[1]}</h2> 
+           <h2>{names[2]}</h2> 
+           
         </div>
     )
 }
