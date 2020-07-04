a5c7be7324b14df6180419d5210bc4d5bf62bd9d
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-07-04 16:28:49 +0530

    Learning conditional rendering 1> If else

--- src/components/UserGreeting.js
+++ src/components/UserGreeting.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react'
 
 class UserGreeting extends Component {
+
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             isLoggedIn: true
+        }
+    }
+    // I want to see the message If isLogged in is true then we have to display Welcome nilank otherwise welcome Guest
     render() {
-        return (
-            <div>
-                
-            </div>
-        )
+        if(this.state.isLoggedIn){
+            return (
+                <div>Welcome Nilank</div>
+            )
+        }else {
+            return (
+                <div>
+                    Welcome Guest
+                </div>
+            )
+        }
+        // return (
+        //     <div>
+        //         <div>Welcome Nilank</div>
+        //         <div>Welcome Guest</div>
+        //     </div>
+        // )
     }
 }
 
