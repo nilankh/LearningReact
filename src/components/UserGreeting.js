cfd313bfe97889d811f7cd00e1074555f614af47
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-07-04 16:35:09 +0530

    Learning conditional rendering 2> element variables

--- src/components/UserGreeting.js
+++ src/components/UserGreeting.js
@@ -11,23 +11,32 @@ class UserGreeting extends Component {
     }
     // I want to see the message If isLogged in is true then we have to display Welcome nilank otherwise welcome Guest
     render() {
+
+
+        // Second method Elements variables approach
+        let message 
+
         if(this.state.isLoggedIn){
-            return (
-                <div>Welcome Nilank</div>
-            )
-        }else {
-            return (
-                <div>
-                    Welcome Guest
-                </div>
-            )
+            message = <div>Welcome Nilank</div>
+        }else{
+            message = <div>Welcome Guests</div>
         }
-        // return (
-        //     <div>
+
+        return <div>{message}</div>
+
+
+        // 1st method of condtional rendering
+        // if(this.state.isLoggedIn){
+        //     return (
         //         <div>Welcome Nilank</div>
-        //         <div>Welcome Guest</div>
-        //     </div>
-        // )
+        //     )
+        // }else {
+        //     return (
+        //         <div>
+        //             Welcome Guest
+        //         </div>
+        //     )
+        // }
     }
 }
 
