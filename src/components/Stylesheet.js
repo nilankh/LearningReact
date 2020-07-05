a69b9df879e6c9970eacc4c0f1d2377a94b43823
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-07-05 20:23:37 +0530

    learning about css style sheets:

--- src/components/Stylesheet.js
+++ src/components/Stylesheet.js
@@ -4,7 +4,8 @@ function Stylesheet(props) {
     let className = props.primary ? 'primary' : ''
     return (
         <div>
-            <h1 className={className}>Style Sheets</h1>
+            {/* <h1 className={className}>Style Sheets</h1> */}
+            <h1 className={`${className} font-xl`}>Style Sheets</h1>
         </div>
     )
 }
