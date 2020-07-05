0088701863d07261583b67512ae95adcac650523
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-07-06 00:17:26 +0530

    implemented 3rd method render method

--- src/components/LifecycleA.js
+++ src/components/LifecycleA.js
@@ -1,31 +1,27 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class LifecycleA extends Component {
+  // step1
+  constructor(props) {
+    super(props);
 
-    // step1
-    constructor(props) {
-        super(props)
-    
-        this.state = {
-             name: 'Nilank'
-        }
-        console.log('LifecycleA constructor');
-        
-    }
-    // step2
-    static getDerivedStateFromProps(props, state){
+    this.state = {
+      name: "Nilank",
+    };
+    console.log("LifecycleA constructor");
+  }
+  // step2
+  static getDerivedStateFromProps(props, state) {
     // this methd get access to props and state as parameters and has to return new state of null
-    console.log('LifecycleA getDerivedStateFromProps');
-    
-    return  null;
-}
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+    console.log("LifecycleA getDerivedStateFromProps");
+
+    return null;
+  }
+  // then third method is render
+  render() {
+    console.log("LifeCycleA render");
+    return <div>LifecycleA</div>;
+  }
 }
 
-export default LifecycleA
+export default LifecycleA;
