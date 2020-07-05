044884f84bbfa4a4c3a5a9d9cd9b82518ffa9771
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-07-05 20:21:46 +0530

    learning about css style sheets:

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
 
   return (
     <div className="App">
-      <Stylesheet />
+      <Stylesheet primary={true} />
       {/* <NameList /> */}
       {/* <NameList2 /> */}
 
--- src/components/Stylesheet.js
+++ src/components/Stylesheet.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import './myStyles.css'
-function Stylesheet() {
+function Stylesheet(props) {
+    let className = props.primary ? 'primary' : ''
     return (
         <div>
-            <h1 className='primary'>Style Sheets</h1>
+            <h1 className={className}>Style Sheets</h1>
         </div>
     )
 }
