d28dbfbfff6e768e3970ea921326e18c0427a18a
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-07-07 21:03:49 +0530

    learning about fragments

--- src/components/FragmentDemo.js
+++ src/components/FragmentDemo.js
@@ -2,10 +2,15 @@ import React from "react";
 
 function FragmentDemo() {
   return (
-    <div>
-      <h1>Fragment Demo</h1>
-      <p>This decribes the Fragment Deomo Component</p>
-    </div>
+    //   when you will inspect then you will see that there is extra div tag, so thats why we have to used fragements
+    // <div>
+    //   <h1>Fragment Demo</h1>
+    //   <p>This decribes the Fragment Deomo Component</p>
+    // </div>
+    <React.Fragment>
+        <h1>Fragment Demo</h1>
+        <p>This Describes the Fragment Demo Component</p>
+    </React.Fragment>
   );
 }
 
