fa332aa00b6b230449e397a840cef11c165f1a9c
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-07-07 21:06:53 +0530

    learning about fragments and created two files and in table js we are going to using column js file

---
+++ src/components/Table.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Table() {
+    return (
+        <table>
+            <tbody>
+                <tr>
+                    
+                </tr>
+            </tbody>
+        </table>
+    )
+}
+
+export default Table
