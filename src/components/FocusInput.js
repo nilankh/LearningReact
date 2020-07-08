dc005d6452b40499c2ca6b169239521e6386f492
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-07-08 21:55:20 +0530

    refs with class components

---
+++ src/components/FocusInput.js
@@ -0,0 +1,15 @@
+// refs with class components
+
+import React, { Component } from 'react'
+
+export class FocusInput extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default FocusInput
