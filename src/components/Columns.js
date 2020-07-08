a7d44a9e8bd59f03594e503b09eef4efda1f13a0
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-07-08 16:33:37 +0530

    started learning about pure component

--- src/components/Columns.js
+++ src/components/Columns.js
@@ -1,13 +1,22 @@
-import React from 'react'
-
+import React from "react";
 
 function Columns() {
-    return (
-        <React.Fragment>
-            <td>Name</td>
-            <td>Nilank</td>
+  const items = [];
+  return (
+
+    // ReactFragment can also be reprsented as <></>
+    <>
+      {/* {items.map((item) => (
+        <React.Fragment key={item.id}>
+          <h1>Title</h1>
+          <p>{item.title}</p>
         </React.Fragment>
-    )
+      ))} */}  
+
+      <td>Name</td>
+      <td>Nilank</td>
+    </>
+  );
 }
 
-export default Columns
+export default Columns;
