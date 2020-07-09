c36550766634a7807a5e9017270bf6ab84294c6c
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-07-09 16:22:01 +0530

    leaning about error handling that jb hmara error kisi ek me h toa baaki sb toa show krna chahiye na ui pe toa islia we have enclose every thing in ErrorBoundary everything means that part which weill be rendered completed

--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -16,6 +16,12 @@ class ErrorBoundary extends Component {
         }
     }
 
+    // this method is pretty much used to log the errors
+    componentDidCatch(error, info){
+        console.log(error)
+        console.log(info)
+        // in development mode react automatically logs every error tot he console
+    }
     render() {
         if(this.state.hasError){
             return <h1>Something went Wrong</h1>
@@ -27,4 +33,6 @@ class ErrorBoundary extends Component {
 
 // hmesha error dikhaea hi q ki react team aisa banaya hi h lekin ek cut button hofa uspe click kroge toa ui pe show krega
 // ErrorBoundary will catch the error and display a fallback ui which in our case is text something went wrong
+
+// the placement of error boundary is completely upto u, you can wrap just the top level component or wrap any nested individual components so that only that component has fallback ui, leaving the rest of ur user interface workingas expected
 export default ErrorBoundary
