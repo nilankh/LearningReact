0d76bbcd337641d908a1f81a1be20302a5b962dc
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-07-09 17:55:43 +0530

    hoc completed

--- src/components/ClickCounter.js
+++ src/components/ClickCounter.js
@@ -1,6 +1,7 @@
 // higher order components(part1)
 import React, { Component } from "react";
-import UpdatedComponent from "./withCounter";
+
+import withCounter from "./withCounter";
 
 class ClickCounter extends Component {
   
@@ -15,4 +16,4 @@ class ClickCounter extends Component {
   }
 }
 
-export default UpdatedComponent(ClickCounter)
+export default withCounter(ClickCounter)
--- src/components/withCounter.js
+++ src/components/withCounter.js
@@ -3,8 +3,9 @@ import React from "react";
 
 // THe Hoc accepts the original component as its parameter the hoc also returns a new component so within the function body let's create a new component
 // the original component is refereed to as wrapped componet
-const withCounter = (OriginalComponent) => {
-  class NewComponent extends React.Component {
+const withCounter = (WrappedComponent) => {
+//   withComponet replaced NewComponent 
+    class WithCounter extends React.Component {
     constructor(props) {
       super(props);
 
@@ -19,14 +20,14 @@ const withCounter = (OriginalComponent) => {
     };
     render() {
       return (
-        <OriginalComponent
+        <WrappedComponent
           count={this.state.count}
           incrementCount={this.incrementCount}
         />
       );
     }
   }
-  return NewComponent;
+  return WithCounter;
 };
 
 export default withCounter;
