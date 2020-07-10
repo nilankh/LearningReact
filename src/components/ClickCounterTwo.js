205fdc9f5be1495a0928fe39d64012f15ea3670a
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-07-10 18:20:47 +0530

    started learning render props

---
+++ src/components/ClickCounterTwo.js
@@ -0,0 +1,14 @@
+// Render props lecture
+import React, { Component } from 'react'
+
+class ClickCounterTwo extends Component {
+    render() {
+        return (
+            <div>
+                <button>Clicked X times</button>
+            </div>
+        )
+    }
+}
+
+export default ClickCounterTwo
