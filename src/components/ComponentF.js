4aa86a4d99520baf4c3372b52da60bbff1ebc2e9
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-07-11 23:58:05 +0530

    working on context part2

---
+++ src/components/ComponentF.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+
+class ComponentE extends Component {
+    render() {
+        return (
+            <div>
+                Component F
+            </div>
+        )
+    }
+}
+
+export default ComponentE
