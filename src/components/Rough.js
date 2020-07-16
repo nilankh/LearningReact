e969be86a4711254684f6ce07bc4837abce58cc9
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-07-16 16:22:01 +0530

    revised about list rednering and list and keys

--- src/components/Rough.js
+++ src/components/Rough.js
@@ -1,26 +1,29 @@
 import React from "react";
-
+import Person from "./Person";
 function Rough() {
-  const names = ["Ajay kumar", "Mala Sinha", "Nilank Nikhil"];
-
-  // 3rdway
-  const nameList = names.map((name, index) => <h2>{name}</h2>);
-  return (
-    <div>
-      {/* 1st way */}
-      {/* <h2>{names[0]}</h2>
-      <h2>{names[1]}</h2>
-      <h2>{names[2]}</h2> */}
-
-      {/* 2nd way */}
-      {/* {
-        names.map(names => <h2>{names}</h2>)
-      } */}
-
-      {/* 3rd way */}
-      {nameList}
-    </div>
-  );
+  const persons = [
+    {
+      id: 1,
+      name: "Ajay Kumar",
+      age: 30,
+      skill: "React",
+    },
+    {
+      id: 2,
+      name: "Mala Sinha",
+      age: 25,
+      skill: "Angular",
+    },
+    {
+      id: 3,
+      name: "Nilank Nikhil",
+      age: 28,
+      skill: "Vue",
+    },
+  ];
+  // const personList = persons.map(personss => <h2>I am {personss.name}. I am {personss.age} years old. I know {personss.skill}</h2>)
+  const personList = persons.map((person) => <Person key={person.id} person={person} />);
+  return <div>{personList}</div>;
 }
 
 export default Rough;
