cb2d75ffc306b34c795cb3082648365b4af3292e
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-07-17 16:43:30 +0530

    revise css in react:

--- src/components/Stylesheet.js
+++ src/components/Stylesheet.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import './myStyles.css'
-function Stylesheet(props) {
+// function Stylesheet() {
+function Stylesheet(props) {    
     let className = props.primary ? 'primary' : ''
     return (
         <div>
+            {/* <h1 className='primary'>Stylesheets</h1> */}
             {/* <h1 className={className}>Style Sheets</h1> */}
+            {/* if u want to use multiple css in then use template literals */}
             <h1 className={`${className} font-xl`}>Style Sheets</h1>
         </div>
     )  
