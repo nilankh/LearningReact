5c49aa1745ec315e6669204856cdccc020b23d98
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-07-21 16:46:23 +0530

    revised higher order components part2

--- src/components/withCounter.js
+++ src/components/withCounter.js
@@ -1,4 +1,4 @@
-// higher order component part2
+// LECTURE34 higher order component part2
 import React from "react";
 
 // THe Hoc accepts the original component as its parameter the hoc also returns a new component so within the function body let's create a new component
