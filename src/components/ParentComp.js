23a54ee3b0403b2f9cf642fcc4c1cee48518838b
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-12-19 09:03:45 +0530

    done

--- src/components/ParentComp.js
+++ src/components/ParentComp.js
@@ -1,40 +1,38 @@
 // Leccture26 pure components and lecture 27
 // import React, { Component, PureComponent } from 'react'
-import React, { Component } from 'react'
-import RegularComp from './RegularComp'
-import PureComp from './PureComp'
-import MemoComp from './MemoComp'
+import React, { Component } from "react";
+import RegularComp from "./RegularComp";
+import PureComp from "./PureComp";
+import MemoComp from "./MemoComp";
 
 class ParentComp extends Component {
-// class ParentComp extends PureComponent {
-    constructor(props) {
-        super(props)
-    
-        this.state = {
-             name: 'Nilank'
-        }
-    }
+  // class ParentComp extends PureComponent {
+  constructor(props) {
+    super(props);
 
-    
-    componentDidMount() {
-        setInterval(() => {
-            this.setState({
-                name: 'Nilank'
-            })
-        }, 2000)
-    }
-    render() {
-        console.log('*********Parent Comp Render******************')
-        return (
-            <div>
-                Parent Component 
-                <MemoComp name={this.state.name}/>
-                {/* <RegularComp name={this.state.name} />
+    this.state = {
+      name: "Nilank",
+    };
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({
+        name: "Nilank",
+      });
+    }, 2000);
+  }
+  render() {
+    console.log("*********Parent Comp Render******************");
+    return (
+      <div>
+        Parent Component
+        <MemoComp name={this.state.name} />
+        {/* <RegularComp name={this.state.name} />
                 <PureComp name={this.state.name} />  */}
-            
-            </div>
-        )
-    }
+      </div>
+    );
+  }
 }
 
-export default ParentComp
+export default ParentComp;
