4221a575887e5c42e94250c9aec0216fe61ba3ec
NILANK NIKHIL <nilanknikhil@gmail.com>
2021-07-07 11:18:02 +0530

    revising

--- src/components/Columns.js
+++ src/components/Columns.js
@@ -4,20 +4,20 @@ import React from "react";
 function Columns() {
   const items = [];
   return (
-
     // ReactFragment can also be reprsented as <></>
+    // If you use <> </> instead of <React.Fragment></React.Fragment> then there is one limitation you can't pass key attribute.
     <>
       {/* {items.map((item) => (
         <React.Fragment key={item.id}>
           <h1>Title</h1>
           <p>{item.title}</p>
         </React.Fragment>
-      ))} */}  
+      ))} */}
 
       <td>Name</td>
       <td>Nilank</td>
     </>
-  );
+  )
 }
 
 export default Columns;
