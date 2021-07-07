8aecb2173995b2f5c90d755dad4235e4d0a1ce12
NILANK NIKHIL <nilanknikhil@gmail.com>
2021-07-07 08:44:45 +0530

    revising

--- src/components/FunctionClick.js
+++ src/components/FunctionClick.js
@@ -5,16 +5,16 @@ import React from 'react'
 // react events are named using camel case rather than lower case
 // with jsx you pass a function as the event handler
 function FunctionClick() {
-    function clickHandler() {
-        console.log("button Clicked");
-        
-    }
-    return (
-        <div>
-            {/* clickhandler we want handler to be a function and not a function call clickHandler() w  */}
-          <button onClick={clickHandler}>Click</button>      
-        </div>
-    )
+  function clickHandler() {
+    console.log('button Clicked')
+  }
+  return (
+    <div>
+      {/* clickhandler we want handler to be a function and not a function call clickHandler() w  */}
+      <button onClick={clickHandler}>Click</button>
+    </div>
+  )
 }
 // eventHandler is a function and not a function call do not add parenthesis
+// q ki agar parenthesis add kar doge toa kya hoga, console open kro bina click ke hi button clicked output aa jyga jo hme ni chahiye
 export default FunctionClick
